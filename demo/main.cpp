c75ec2e11456827d7f9368e9a69fc00764c30cb4
GoldenGAAAR <p24x8A55Ozx1>
2021-10-22 15:38:48 +0300

    кekes

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,48 +1,29 @@
-#include <iostream>
-#include <nlohmann/json.hpp>
 #include <student.hpp>
-#include <fstream>
 
-void print(const Student& student, std::ostream& os) {
-  //...
-  if (student.debt.type() == typeid(std::nullptr_t)) {
-    os << "null";
-  } else if (student.debt.type() == typeid(std::string)) {
-    os << std::any_cast<std::string>(student.debt);
-  } else {
-    os
-        << std::any_cast<std::vector<std::string> >(student.debt).size()
-        << " items";
-  }
-}
-
-void print(const std::vector<Student>& students, std::ostream& os) {
-
-  //...
-  for (auto const& student : students) {
-    print(student, os);
-  }
-}
-
-int main() {
-  //...
+int main(int argc, char* argv[]) {
   std::string jsonPath;
+  if (argc < 2){
+    throw std::runtime_error("Path is not exist");
+  } else {
+    jsonPath = argv[1];
+  }
+
   std::ifstream file{jsonPath};
   if (!file) {
     throw std::runtime_error{"unable to open json: " + jsonPath};
   }
 
-  nlohmann::json data;
+  json data;
   file >> data;
-std::vector<Student> read_file(nlohmann::json data) {
-  std::vector<Student> students;
-  for (auto const& item : data.at("items")) {
-    Student student;
-    from_json(item, student);
-    students.push_back(student);
+
+  if (data["_meta"]["count"] != data["items"].size()){
+    throw std::runtime_error{"_meta doesn't equal number of students"};
   }
-  return students;
-}
-  //...
+
+  if (!data["items"].is_array()){
+    throw std::runtime_error{"items is not array"};
+  }
+
+  std::vector<Student> students = read_file(data);
   print(students, std::cout);
 }
